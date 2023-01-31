e614101ebc56f30c15a3f2eac1705ef95aa56941
ZahraaTayyar <zahraatayyar@gmail.com>
2023-01-31 17:13:38 +0000

    level 200 completed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function setup() {
   const allEpisodes = getAllEpisodes();
   // makePageForEpisodes(allEpisodes);
-  displayEpisodes(allEpisodes);
+  displayEpisodeWithSearchBox(allEpisodes);
 }
 
 // function makePageForEpisodes(episodeList) {
@@ -12,7 +12,56 @@ function setup() {
 
 window.onload = setup;
 
+
+//initial variable (global scope)
 let list = document.createElement("ul");
+
+let count = 0;
+
+const searchInput = document.createElement("input");
+searchInput.type = "text";
+searchInput.placeholder = "Search";
+
+let countSpan = document.createElement("span");
+document.body.appendChild(searchInput);
+document.body.appendChild(countSpan);
+
+
+function displayEpisodeWithSearchBox(episodes) {
+  displayEpisodes(episodes);
+
+  countSpan.innerHTML = `Displaying ${count}/${episodes.length} episode(s)`
+
+  searchInput.addEventListener("input", () => {
+    const inputValue = searchInput.value.toLowerCase();
+    list.innerHTML = "";
+    count = 0;
+
+    episodes.forEach((episode) => {
+      if (episode.name.toLowerCase().includes(inputValue) || episode.summary.toLowerCase().includes(inputValue)) {
+        let li = document.createElement("li");
+        let title = document.createElement("h1");
+        let image = document.createElement("img");
+        let paragraph = document.createElement("p");
+
+        let episodeCode = `S${("0" + episode.season).slice(-2)}E${("0" + episode.number).slice(-2)}`
+        title.innerHTML = `${episode.name} - ${episodeCode}`;
+        image.src = episode.image.medium;
+        paragraph.innerHTML = episode.summary;
+
+        document.body.appendChild(list);
+        list.appendChild(li);
+        li.appendChild(title);
+        li.appendChild(image);
+        li.appendChild(paragraph);
+        count++;
+
+      }
+  })
+  countSpan.innerHTML = `Displaying ${count}/${episodes.length} episode(s)`
+  })
+}
+
 
 function displayEpisodes(episodes) {
   episodes.forEach((episode) => {
@@ -31,5 +80,6 @@ function displayEpisodes(episodes) {
     li.appendChild(title);
     li.appendChild(image);
     li.appendChild(paragraph);
+    count++;
   })
 }
