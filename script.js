c5bb57574cc9b02db3e0fc4ed23a5c98f8585510
ZahraaTayyar <zahraatayyar@gmail.com>
2023-02-10 18:43:59 +0000

    level 350 completed

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
+//level 350 - Get episodes from API
+const url = "https://api.tvmaze.com/shows/82/episodes";
+let allEpisodes = [];
+
+
 //You can edit ALL of the code here
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  // makePageForEpisodes(allEpisodes);
-  displayEpisodeWithSearchBox(allEpisodes);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      allEpisodes = data;
+      displayEpisodeWithSearchBox(allEpisodes);
+    })
+    .catch((err) => console.log(err));
 }
-
-// function makePageForEpisodes(episodeList) {
-//   const rootElem = document.getElementById("root");
-//   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-// }
 
 window.onload = setup;
 
@@ -18,18 +22,22 @@ let list = document.createElement("ul");
 
 let count = 0;
 
+//variable to choose episodes from a list
 const select = document.createElement("select");
 const allOption = document.createElement("option");
 document.body.appendChild(select);
 
+//variable to input text for the search function
 const searchInput = document.createElement("input");
 searchInput.type = "text";
 searchInput.placeholder = "Search";
 
+//variable to show number of episodes displayed
 let countSpan = document.createElement("span");
 document.body.appendChild(searchInput);
 document.body.appendChild(countSpan);
 
+//put input tab into one div so can style properly
 let inputDiv = document.createElement("div");
 inputDiv.className = "input-div";
 document.body.appendChild(inputDiv);
@@ -39,8 +47,30 @@ inputDiv.appendChild(searchInput);
 inputDiv.appendChild(countSpan);
 
 
+//level 100 - function for episodes to be displayed
+function displayEpisodes(episodes) {
+  episodes.forEach((episode) => {
+    let li = document.createElement("li");
+    let title = document.createElement("h1");
+    let image = document.createElement("img");
+    let paragraph = document.createElement("p");
 
-//function for search input
+    let episodeCode = `S${("0" + episode.season).slice(-2)}E${("0" + episode.number).slice(-2)}`
+    title.innerHTML = `${episode.name} - ${episodeCode}`;
+    image.src = episode.image.medium;
+    paragraph.innerHTML = episode.summary;
+
+    document.body.appendChild(list);
+    list.appendChild(li);
+    li.appendChild(title);
+    li.appendChild(image);
+    li.appendChild(paragraph);
+    count++;
+  })
+}
+
+
+//level 200 - function for search input
 function displayEpisodeWithSearchBox(episodes) {
   displayEpisodes(episodes);
   displayDropBox(episodes);
@@ -78,30 +108,7 @@ function displayEpisodeWithSearchBox(episodes) {
 }
 
 
-//function for episodes to be displayed
-function displayEpisodes(episodes) {
-  episodes.forEach((episode) => {
-    let li = document.createElement("li");
-    let title = document.createElement("h1");
-    let image = document.createElement("img");
-    let paragraph = document.createElement("p");
-
-    let episodeCode = `S${("0" + episode.season).slice(-2)}E${("0" + episode.number).slice(-2)}`
-    title.innerHTML = `${episode.name} - ${episodeCode}`;
-    image.src = episode.image.medium;
-    paragraph.innerHTML = episode.summary;
-
-    document.body.appendChild(list);
-    list.appendChild(li);
-    li.appendChild(title);
-    li.appendChild(image);
-    li.appendChild(paragraph);
-    count++;
-  })
-}
-
-
-//DropBox search bar
+//level 300 - DropBox search bar
 function displayDropBox(episodes) {
   allOption.value = "All";
   allOption.innerHTML = "All episodes";
@@ -132,3 +139,4 @@ function displayDropBox(episodes) {
     })
   })
 }
+
