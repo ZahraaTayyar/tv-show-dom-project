cb54b3638a2b59b9c275b1e263282bf3900d36aa
ZahraaTayyar <zahraatayyar@gmail.com>
2023-02-28 16:47:41 +0000

    fixed bug for episode selector

--- script.js
+++ script.js
@@ -23,8 +23,6 @@ function setup() {
     .catch((err) => console.log(err));
 }
 
-window.onload = setup;
-
 
 /**
  * GLOBAL VARIABLES
@@ -45,93 +43,73 @@ const selectEpisodesEl = document.querySelector(".select-episodes");
  */
 
 //level 200 - function for search input
-function displayEpisodeWithSearchBox(episodes) 
-{
+function displayEpisodeWithSearchBox(episodes) {
   displayDropBox(episodes);
-
-  countSpan.innerHTML = `Displaying ${count}/${episodes.length} episode(s)`;
-
   searchInput = document.querySelector('.search-input');
-
-  searchInput.addEventListener("input", () => 
-  {
+  searchInput.addEventListener("input", () => {
     const inputValue = searchInput.value.toLowerCase();
     list.innerHTML = "";
     count = 0;
-
-    episodes.forEach((episode) => 
-    {
+    episodes.forEach((episode) => {
       if (episode.name.toLowerCase().includes(inputValue) || episode.summary.toLowerCase().includes(inputValue)) 
       {
         makeEpisodeCard(list, episode);
       }
     })
-  countSpan.innerHTML = `Displaying ${count}/${episodes.length} episode(s)`
+  displayEpisodeCount()
   })
 }
 
-
 //level 300 - Episode DropDown search bar
-function displayDropBox(episodes) 
-{
+function displayDropBox(episodes) {
   const allOption = document.querySelector(".episode-option");
   allOption.value = "All";
 
-  episodes.forEach((episode) => 
-  {
+  episodes.forEach((episode) => {
     const eachOption = document.createElement("option");
     eachOption.value = episode.id;
+    let episodeCode = `S${("0" + episode.season).slice(-2)}E${("0" + episode.number).slice(-2)}`;
+    eachOption.innerHTML = `${episodeCode} - ${episode.name}`;
+    selectEpisodesEl.appendChild(eachOption)
 
-    makeMediaTitleV2(selectEpisodesEl, episode);
-
-    selectEpisodesEl.addEventListener("change", () => 
-    {
+    selectEpisodesEl.addEventListener("change", () => {
       let newArr = [];
-
       if (selectEpisodesEl.value === "All") {
         newArr = episodes;
       } else {
         //something is wrong with the below part of the if statement.
-        newArr = episodes.filter(episode => selectEpisodesEl.value.includes(episode.id));
+        newArr = episodes.filter((episode) => selectEpisodesEl.value.includes(episode.id));
       }
       // count = 0;
       list.innerHTML = "";
       displayEpisodes(newArr);
-      countSpan.innerHTML = `Displaying ${count}/${episodes.length} episode(s)`
+      displayEpisodeCount();
     })
   })
 }
 
 //level 400 - Shows dropbox
-
-function displayShowsDropbox(shows) 
-{
+function displayShowsDropbox(shows) {
   const selectShowsOptions = document.querySelector(".show-option");
   selectShowsOptions.value = "All";
 
-  shows.forEach((show) => 
-  {
+  shows.forEach((show) => {
     const selectShowsOption = document.createElement("option");
     selectShowsOption.value = show.id;
-
     const showName = show.name;
     selectShowsOption.innerHTML = showName;
     selectShowsEl.appendChild(selectShowsOption);
 
-    selectShowsEl.addEventListener("change", () => 
-    {
+    selectShowsEl.addEventListener("change", () => {
       let newArr = [];
-
       if (selectShowsEl.value === "All") {
         newArr = shows;
       } else {
         newArr = shows.filter(show => selectShowsEl.value.includes(show.id))
       }
-
       list.innerHTML = "";
       displayShows(newArr);
-
-      countSpan.innerHTML = `Displaying ${count}/${shows.length} show(s)`;
+      displayShowCount();
     })
 })
 }
@@ -141,36 +119,30 @@ function displayShowsDropbox(shows)
    * HELPER FUNCTIONS
    */
 
-
-  function displayShows(shows)
-  {  
+  function displayShows(shows) {  
     count = 0;
     shows.forEach((show) => 
     {
       makeShowCard(list, show);
     })
-    countSpan.innerHTML = `Displaying ${count}/${shows.length} show(s)`
+    displayShowCount();
   }
 
-  function displayEpisodes(episodes) 
-  {  
+  function displayEpisodes(episodes) {  
     count = 0;
     episodes.forEach((episode) => 
     {
       makeEpisodeCard(list, episode);
     })
+    count++;
+    displayEpisodeCount()
   }
 
-  function makeEpisodeCard(el, episode) 
-  {
+  function makeEpisodeCard(el, episode) {
     let li = document.createElement("li");
-
     makeEpisodeTitle(li, episode);
-
     makeCardImage(li, episode, 'episodes-image');
-
     makeCardSummary(li, episode);
-
     document.body.appendChild(el);
     el.appendChild(li);
     count++;
@@ -178,83 +150,54 @@ function displayShowsDropbox(shows)
 
   function makeShowCard(el, show) {
     let li = document.createElement("li");
-
     makeShowTitle(li, show);
-
     makeCardImage(li, show, 'shows-image');
-
     makeCardSummary(li, show);
-
     document.body.appendChild(el);
     el.appendChild(li);
     count++;
   }
 
-  function makeEpisodeTitle(el, episode) 
-  {
+  function makeEpisodeTitle(el, episode) {
     let title = document.createElement("h1");
-
     let episodeCode = `S${("0" + episode.season).slice(-2)}E${("0" + episode.number).slice(-2)}`;
-
     title.innerHTML = `${episode.name} - ${episodeCode}`;
-
     el.appendChild(title);
   };
 
-  function makeShowTitle(el, show) 
-  {
+  function makeShowTitle(el, show) {
     let title = document.createElement("h1");
     title.innerHTML = `${show.name}`;
     el.appendChild(title);
   }
 
-  function makeMediaTitleV2(el, media) 
-  {
-    let episodeCode = `S${("0" + media.season).slice(-2)}E${("0" + media.number).slice(-2)}`;
-
-    const eachOption = document.createElement("option");
-
-    eachOption.innerHTML = `${episodeCode} - ${media.name}`;
-
-    el.appendChild(eachOption)
-  };
-
-  function makeCardImage(el, media, className) 
-  {
+  function makeCardImage(el, media, className) {
     let image = document.createElement("img");
     image.src = media.image.medium;
     image.className = `${className}`;
     el.appendChild(image);
   }
 
-  function makeCardSummary(el, media) 
-  {
+  function makeCardSummary(el, media) {
     let paragraph = document.createElement("p");
-
     if (media.summary.length > 130) {
       paragraph.innerHTML = `${media.summary.substr(0, 130)}...`;
     } else {
       paragraph.innerHTML = media.summary;
     }
-
     el.appendChild(paragraph);
   }
 
-  function getUrlFromId(id) 
-  {
+  function getUrlFromId(id) {
     return `https://api.tvmaze.com/shows/${id}/episodes`;
   }
 
-
-  //functions for count to be displayed - yet to be used - breaks code each time i use it, need to fix
-
-  function displayEpisodeCount() 
-  {
-    countSpan.innerHTML = `Displaying ${count}/${episodes.length} episode(s)`;
+  function displayEpisodeCount() {
+    countSpan.innerHTML = `Displaying ${count} episode(s)`;
   }
 
   function displayShowCount() {
-    countSpan.innerHTML = `Displaying ${count}/${shows.length} show(s)`;
+    countSpan.innerHTML = `Displaying ${count} show(s)`;
   }
 
 
@@ -266,3 +209,5 @@ function displayShowsDropbox(shows)
 //   const showIdSelectedByUser = Number(e.target.value);
 //   const nextFetchUrl = getUrlFromId(showIdSelectedByUser);
 // })
+
+window.onload = setup;
